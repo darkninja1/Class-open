afa44b8f9a6247b844de86ffc68e6db89c64c84d
Dominik Honzak <>
2021-05-12 15:35:06 +0000

    ...

--- main.js
+++ main.js
@@ -24,3 +24,30 @@ function dayw() {
   }
 
 }
+function odd_day() {
+  confirm("Proceed with odd day?");
+  setInterval(showTime, 1000);
+}
+function even_day() {
+  confirm("Proceed with even day?");
+  setInterval(showTime2, 1000);
+}
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction123() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
