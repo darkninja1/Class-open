d613fac422bec50d464dd6e177fda4b3d8869579
Dominik Honzak <49328567+darkninja1@users.noreply.github.com>
2021-12-21 22:24:03 +0000

    ...

--- main.js
+++ main.js
@@ -38,7 +38,9 @@ function shuffle() {
     var song = songs[number];
     //length = song.getMilliseconds();
     //document.getElementById('day4').innerHTML = length;
+    document.getElementById('day2').style.backgroundPosition = 'center center';
     document.getElementById('day2').style.backgroundImage = "url('"+pics[number]+"')";
+    
     document.getElementById('day2').innerHTML = "Playing: "+songn[number];
     pre.push(songn[number]);
     document.getElementById('musicid').src = song;
